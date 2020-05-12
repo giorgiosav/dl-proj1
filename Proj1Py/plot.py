2d8c58eb181ecc75a74873c6e4841f75bd4cbe56
manuleo <manuel.leone97@gmail.com>
2020-05-12 12:23:18 +0200

    Comment plots

--- Proj1Py/plot.py
+++ Proj1Py/plot.py
@@ -8,13 +8,13 @@ import torch
 # Used to save in LaTeX design
 # This configuration has been commented to make the implementation work in the VM.
 # The plot are reproducible even without it, but they won't have the "LaTeX style"
-matplotlib.use("pgf")
-matplotlib.rcParams.update({
-    "pgf.texsystem": "pdflatex",
-    'font.family': 'serif',
-    'text.usetex': True,
-    'pgf.rcfonts': False,
-})
+# matplotlib.use("pgf")
+# matplotlib.rcParams.update({
+#     "pgf.texsystem": "pdflatex",
+#     'font.family': 'serif',
+#     'text.usetex': True,
+#     'pgf.rcfonts': False,
+# })
 
 
 def plot_over_epochs(values_list: list, epochs: int, label: str, savename: str):
